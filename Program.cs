54c9983e847de753c98aa167e33439d510b0bf1f
NataliyaNataliya <65646740+NataliyaNataliya@users.noreply.github.com>
2020-05-26 12:10:31 +0300

    edit something

--- Program.cs
+++ Program.cs
@@ -17,6 +17,7 @@ namespace WinForms25052020Test1
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            //edit something
         }
     }
 }
