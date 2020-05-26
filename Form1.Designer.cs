8036369519b8b80604882b02fd2b7454a3316b5d
NataliyaNataliya <tnvtyvtaya@gmail.com>
2020-05-26 11:24:53 +0300

    add button - random strings

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.addRndStrButton3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listBox1
@@ -59,11 +60,22 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // addRndStrButton3
+            // 
+            this.addRndStrButton3.Location = new System.Drawing.Point(516, 292);
+            this.addRndStrButton3.Name = "addRndStrButton3";
+            this.addRndStrButton3.Size = new System.Drawing.Size(141, 45);
+            this.addRndStrButton3.TabIndex = 3;
+            this.addRndStrButton3.Text = "Add Random Strings";
+            this.addRndStrButton3.UseVisualStyleBackColor = true;
+            this.addRndStrButton3.Click += new System.EventHandler(this.addRndStrButton3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.addRndStrButton3);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.listBox1);
@@ -79,6 +91,7 @@
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button addRndStrButton3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,17 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WinForms25052020Test1
 {
     public partial class Form1 : Form
     {
+        Random random = new Random();
         public Form1()
         {
             InitializeComponent();
@@ -20,6 +16,26 @@ namespace WinForms25052020Test1
         private void button1_Click(object sender, EventArgs e)
         {
             listBox1.Items.Add(textBox1.Text);
+        }
+
+        private void addRndStrButton3_Click(object sender, EventArgs e)
+        {
+            List<string> list = new List<string>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                list.Add(Rnd());
+            }
+
+            listBox1.DataSource = list;
+        }
+
+        public string Rnd()
+        {
+            string str = "";
+            for (int i = 0; i < 10; i++)
+                str += (char)random.Next(97, 122);
+            return str;
         }
     }
 }
