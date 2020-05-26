ffa774743544c0bee248597100d97f34a9c09122
NataliyaNataliya <tnvtyvtaya@gmail.com>
2020-05-26 09:51:14 +0300

    changed the colors of the form and controls

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,6 +36,7 @@
             // 
             // listBox1
             // 
+            this.listBox1.BackColor = System.Drawing.Color.Violet;
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 16;
             this.listBox1.Location = new System.Drawing.Point(12, 12);
@@ -45,6 +46,7 @@
             // 
             // textBox1
             // 
+            this.textBox1.BackColor = System.Drawing.Color.Violet;
             this.textBox1.Location = new System.Drawing.Point(391, 12);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(397, 22);
@@ -52,28 +54,31 @@
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.Violet;
             this.button1.Location = new System.Drawing.Point(516, 90);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(141, 45);
             this.button1.TabIndex = 2;
             this.button1.Text = "Add";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // cleanButton2
             // 
+            this.cleanButton2.BackColor = System.Drawing.Color.Violet;
             this.cleanButton2.Location = new System.Drawing.Point(516, 192);
             this.cleanButton2.Name = "cleanButton2";
             this.cleanButton2.Size = new System.Drawing.Size(141, 45);
             this.cleanButton2.TabIndex = 3;
             this.cleanButton2.Text = "Clean";
-            this.cleanButton2.UseVisualStyleBackColor = true;
+            this.cleanButton2.UseVisualStyleBackColor = false;
             this.cleanButton2.Click += new System.EventHandler(this.cleanButton2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lime;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.cleanButton2);
             this.Controls.Add(this.button1);
